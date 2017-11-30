65a3cb3fb8adea27733a1381da7812a05c5d83cd
ilyiglesias <ilysa.iglesias@noaa.gov>
2017-11-30 12:11:51 -0800

    new Rstudio project

---
+++ day1_git.R
@@ -0,0 +1,13 @@
+# How to use GIT hub from within RStudio 
+
+# ensure that GIT points proper version and username (in terminal)
+# browse: usr/local/git/bin/git in tools global options (can use which in terminal to set to that location)
+# by creating new project in Rstudio-- downloades all files in your repository 
+#.gitignore - keeps track of command history 
+
+getwd() #[1] "/Users/ilyalreck/Desktop/MBARIR/mbari_git"
+setwd() # tells R where to look 
+list.files() # shows you piles in working directory project sets your working directory 
+# tools //  project options // text file 
+
+# Git tab 
