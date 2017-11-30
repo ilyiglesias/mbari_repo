bfdf8cd18ede293ce9be9a2c946ccc2664d25ee5
ilyiglesias <ilysa.iglesias@noaa.gov>
2017-11-30 14:43:25 -0800

    still... trying to fix error

--- docs/ggplot.R
+++ docs/ggplot.R
@@ -6,3 +6,5 @@ library(tidyverse)
 
 # grammer of graphics- each row is an observation and each variable is a column 
     # dplyr::filter() filter function in both stats {base} and dplyr
+
+# mapping= aes() 
